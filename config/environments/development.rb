f364c19053aba7d81eb99e69f5f235b97904ddf4
Andres Montano <amontano@virginia.edu>
2020-05-31 19:37:25 -0600

    updating active_resource_extensions to 2.2.1, authenticated_system to 2.3.9, complex_dates to 2.2.9, flare to 1.2.8, kmaps_engine to 5.8.8, mms_integration to 2.4.0, places_integration to 2.2.2, shanti_integration to 3.4.7, and subjects_engine to 1.5.0

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,6 +29,9 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
+  
+  # When doing recordings importation using forks the asyncjob gets stuck if not runnign inline
+  config.active_job.queue_adapter = :delayed_job
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_04_234543) do
+ActiveRecord::Schema.define(version: 2020_05_26_201235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -121,6 +121,23 @@ ActiveRecord::Schema.define(version: 2019_01_04_234543) do
     t.integer "rabjung_end_id"
     t.integer "intercalary_month_end_id"
     t.integer "intercalary_day_end_id"
+  end
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string "locked_by"
+    t.string "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "reference_id"
+    t.string "reference_type"
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
   create_table "descriptions", id: :serial, force: :cascade do |t|
